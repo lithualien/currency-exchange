3b911e7654c83494ceb9f747440af41a47b34b14
lithualien <tomas.dominauskas@protonmail.com>
2020-08-20 23:12:22 +0300

    initial commit.

---
+++ src/main/java/com/github/lithualien/currencyexchanger/CurrencyExchangerApplication.java
@@ -0,0 +1,13 @@
+package com.github.lithualien.currencyexchanger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurrencyExchangerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CurrencyExchangerApplication.class, args);
+    }
+
+}
