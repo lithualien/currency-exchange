f149c4aa9d734e9eb7b82dc2a7b8e257d31879fe
lithualien <tomas.dominauskas@protonmail.com>
2020-08-25 13:26:55 +0300

    The end of the story.

---
+++ src/main/java/com/github/lithualien/currencyexchanger/commands/v1/DateCommand.java
@@ -0,0 +1,17 @@
+package com.github.lithualien.currencyexchanger.commands.v1;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class DateCommand {
+
+    private LocalDate date;
+}
--- src/main/java/com/github/lithualien/currencyexchanger/controllers/CurrencyController.java
+++ src/main/java/com/github/lithualien/currencyexchanger/controllers/CurrencyController.java
@@ -3,6 +3,7 @@ package com.github.lithualien.currencyexchanger.controllers;
 import com.github.lithualien.currencyexchanger.commands.v1.CurrencyCommand;
 import com.github.lithualien.currencyexchanger.commands.v1.CurrencyInputCommand;
 import com.github.lithualien.currencyexchanger.commands.v1.CurrencyOutputCommand;
+import com.github.lithualien.currencyexchanger.commands.v1.DateCommand;
 import com.github.lithualien.currencyexchanger.services.CurrencyNameService;
 import com.github.lithualien.currencyexchanger.services.CurrencyRateService;
 import lombok.extern.log4j.Log4j2;
@@ -10,6 +11,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @Log4j2
@@ -36,6 +38,13 @@ public class CurrencyController {
     public ResponseEntity<CurrencyOutputCommand> getCurrencyPrice(@RequestBody CurrencyInputCommand input) {
         CurrencyOutputCommand currencyOutputCommand = currencyRateService.getCurrencyValue(input);
         return new ResponseEntity<>(currencyOutputCommand, HttpStatus.OK);
+    }
+
+    @GetMapping("/today")
+    public ResponseEntity<DateCommand> getDate() {
+        DateCommand dateCommand = new DateCommand(LocalDate.now());
+        log.info("Fetched date.");
+        return new ResponseEntity<>(dateCommand, HttpStatus.OK);
     }
 
 }
